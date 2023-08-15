07d92abbd7dd172f2563ba0cb8e796ef5a527ffb
odinthenord <140075322+odinthenord@users.noreply.github.com>
2023-08-15 13:05:42 +0530

    code works with single digit in order(firstNo,opr,secNo)

--- calc.js
+++ calc.js
@@ -14,9 +14,10 @@ function divide(a,b) {
     return a/b;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = null;
+let secondNumber = null;
+let currentOperator = null;
+let clicked = false;
 
 function operate(number1,operation,number2) {
     if(operation === '+') {
@@ -33,11 +34,33 @@ function operate(number1,operation,number2) {
     }
 }
 
-const screen = document.querySelector('#screen');
+const operators = document.querySelectorAll('.operator');
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-    button.addEventListener('click',()=>{
-        screen.textContent += button.innerText;
-    })
+operators.forEach(operator => {
+    operator.addEventListener('click', ()=> {
+        currentOperator = operator.value;
+        console.log(currentOperator);
+        clicked = true;
+    });
+});
+
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if(clicked === true) {
+            secondNumber = parseInt(number.value);
+            console.log(`secondnumber: ${secondNumber}`);
+        }else {
+            firstNumber = parseInt(number.value);
+            console.log(`firstnumber: ${firstNumber}`);
+        }
+    });
+});
+
+const equalTo = document.querySelector('.equals');
+
+equalTo.addEventListener('click', ()=> {
+    console.log(operate(firstNumber,currentOperator,secondNumber));
+    clicked = false;
 });
